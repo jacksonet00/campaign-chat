8d69b0fcb38ccad0f6965d7c99c655069b7fd194
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-01-29 02:21:14 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,43 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  const [chatHistory, setChatHistory] = useState<string[]>([]);
+type Chat = {
+  message: string;
+  sender: boolean;
+  id: string;
+};
 
-  useEffect(() => {}, []);
+const Home: NextPage = () => {
+  const [chatHistory, setChatHistory] = useState<Chat[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [input, setInput] = useState("");
+
+  function addChat(chat: Chat) {
+    setLoading(true);
+    setChatHistory([chat, ...chatHistory]);
+    setLoading(false);
+  }
+
+  function initializeGame() {
+    setLoading(true);
+    addChat({
+      message: "welcome to your campaign",
+      sender: false,
+      id: "1",
+    });
+    setLoading(false);
+  }
+
+  function resetGame() {
+    setLoading(true);
+    localStorage.removeItem("campaignChatHistory");
+    setChatHistory([]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    initializeGame();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -22,12 +55,54 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>Campaign Chat</h1>
         <section className={styles.chatBox}>
-          <article className={styles.chat}>
-            <p className={styles.chatMessage}>chat message</p>
-          </article>
+          {loading ? (
+            <p>loading...</p>
+          ) : (
+            <>
+              {chatHistory.map((chat) => (
+                <article
+                  className={
+                    chat.sender ? styles.chatMessageTo : styles.chatMessageFrom
+                  }
+                  key={chat.id}
+                >
+                  <p>{chat.message}</p>
+                </article>
+              ))}
+            </>
+          )}
+        </section>
+
+        <section
+          className={styles.inputContainer}
+          onSubmit={(e) => {
+            e.preventDefault();
+            addChat({
+              message: input,
+              sender: true,
+              id: "2",
+            });
+            setInput("");
+          }}
+        >
+          <form>
+            <input
+              type="text"
+              placeholder="..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <input type="submit" />
+          </form>
+          <button
+            className={chatHistory.length > 0 ? styles.end : styles.start}
+            onClick={chatHistory.length > 0 ? resetGame : initializeGame}
+          >
+            {chatHistory.length > 0 ? "End Game" : "Start Game"}
+          </button>
         </section>
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://github.com/jacksonet00/campaign-chat"
           target="_blank"
@@ -38,7 +113,7 @@ const Home: NextPage = () => {
             <Image src="/heart.png" alt="Heart emoji" width={30} height={30} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
